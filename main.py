e8a9e9491eb7f330724414df18a75db83a339542
Subho Ghosh <ighoshsubho@gmail.com>
2023-09-20 11:02:14 +0530

    Bug fix: Docker fail

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from app.helpers import chatbotQA
-
+from app import helpers
 app = FastAPI()
 
 @app.post("/api/")
 async def create_item(text:str):
-    res = chatbotQA(text)
+    res = helpers.chatbotQA(text)
     return {"response":res}
